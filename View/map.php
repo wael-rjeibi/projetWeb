544f51a31e8cca544258fada68522924ff8d6cd5
WaelRjeibi2A18 <80390442+wael-rjeibi@users.noreply.github.com>
2022-05-08 21:29:01 +0100

    Delete View directory

--- View/map.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST["submit1"]))
-{
-    $adresse=$_POST["adresse"];
-    ?>
-    <iframe widht="1000%" height="500" src="https://maps.google.com/maps?q=<?php echo $adresse ; ?>&output=embed"> </iframe> 
-<?php }
-?>
